3c05b92b1d5e048e9a0beb9dfa3e8b883d57b668
Ankit Arora <05.ankitarora@gmail.com>
2018-02-19 00:52:11 +0530

    Changes for wor

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var app = express();
 var server = require('http').createServer(app);
 var path = require('path');
 
-server.listen(process.env.PORT || 9000);
+server.listen(process.env.PORT || 8080);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-console.log('Server running on 9000');
+console.log('Server running on 8080');
